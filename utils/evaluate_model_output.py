980aed22a4f42e85f4bcfb071ccc762b8ff771ab
So Yeon Min <soyeonmin@Sos-MacBook-Pro.local>
2024-07-09 15:04:09 -0400

    m

--- utils/evaluate_model_output.py
+++ utils/evaluate_model_output.py
@@ -39,7 +39,6 @@ with gzip.open(task_json_path, 'rb') as f:
 
 amb_success_list = []
 amb_fail_idxes = []
-breakpoint()
 for ep_idx, episode in enumerate(task_json_data['episodes']):
 	ambiguous = episode['sif_params']['ambiguous']
 	success = success_list[ep_idx] 
@@ -55,6 +54,6 @@ for ep_idx, episode in enumerate(task_json_data['episodes']):
 
 #Just see for ambiguous  episodes
 print("SR is ", np.mean(list(success_list.values())))
-print("AMB SR is ", np.mean(amb_success_list))
-print("Amb len ", len(amb_success_list))
+# print("AMB SR is ", np.mean(amb_success_list))
+# print("Amb len ", len(amb_success_list))
 print(amb_fail_idxes)
