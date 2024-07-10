7904b42162193f104c6cdc5dcac7a4c6962773d7
So Yeon Min <soyeonmin@sos-macbook-pro.wifi.local.cmu.edu>
2024-07-10 15:43:48 -0400

    m

--- utils/evaluate_model_output.py
+++ utils/evaluate_model_output.py
@@ -9,6 +9,7 @@ import numpy as np
 parser = argparse.ArgumentParser(description="")
 parser.add_argument('--exp_name',  type=str, required=True) 
 parser.add_argument('--json_name',  type=str, required=True)
+parser.add_argument('--get_amb_clear', action='store_true', default=False)
 args = parser.parse_args()
 
 #Just get SR
@@ -46,3 +47,41 @@ for k, v in success_list.items():
 	if not(v):
 		fail_idx.append(k)
 print("Failed idxes are ", fail_idx)
+
+#Also print by ambiguous/ clear
+if args.get_amb_clear:
+	amb_success_list = []
+	clear_success_list = []
+	amb_spl_list = []
+	clear_spl_list = []
+
+	clear_idxes = []
+	amb_idxes = []
+
+	for ep_idx, episode in enumerate(task_json_data['episodes']):
+		if ep_idx in indices_to_include:
+			ambiguous = episode['sif_params']['ambiguous']
+			success = success_list[ep_idx] 
+			spl = spl_list[ep_idx] 
+			if ambiguous:
+				amb_success_list.append(success)
+				amb_spl_list.append(spl)
+				amb_idxes.append(ep_idx)
+			else:
+				clear_success_list.append(success)
+				clear_spl_list.append(spl)
+				clear_idxes.append(ep_idx)
+
+	print("amb + clear len ", len(amb_success_list + clear_success_list))
+
+	print("clear len ", len(clear_success_list))
+	print("amb len ", len(amb_success_list))
+
+	print("CLEAR SR is ", np.mean(clear_success_list))
+	print("CLEAR SPL is ", np.mean(clear_spl_list))
+
+	print("AMB SR is ", np.mean(amb_success_list))
+	print("AMB SPL is ", np.mean(amb_spl_list))
+
+	print("Amb idxes ", amb_idxes)
+	print("Clear idxes ", clear_idxes)	
